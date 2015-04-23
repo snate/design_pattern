f42283366770473eb618791252b45d770321d14e
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-04-23 15:51:27 +0200

    Abstract Factory: Add first family of Products

---
+++ Abstract_Factory/clothes/RedPairOfShoes.java
@@ -0,0 +1,12 @@
+/**
+  * Sebastiano Valle - 23/04/2015
+  * This is the class which represents GoF's ``ProductA2" class.
+*/
+package clothes;
+
+public class RedPairOfShoes implements PairOfShoes {
+
+  public void print() {
+    System.out.println("Red boots");
+  }
+}
---
+++ Abstract_Factory/clothes/RedShirt.java
@@ -0,0 +1,12 @@
+/**
+  * Sebastiano Valle - 23/04/2015
+  * This is the class which represents GoF's ``ProductA1" class.
+*/
+package clothes;
+
+public class RedShirt implements Shirt {
+
+  public void print() {
+    System.out.println("Shirt with a tomato");
+  }
+}
