bb4f54597644a39f936940e09976ed248c284526
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-04-23 15:51:48 +0200

    Abstract Factory: Add second family of Products

---
+++ Abstract_Factory/clothes/BluePairOfShoes.java
@@ -0,0 +1,12 @@
+/**
+  * Sebastiano Valle - 23/04/2015
+  * This is the class which represents GoF's ``ProductB2" class.
+*/
+package clothes;
+
+public class BluePairOfShoes implements PairOfShoes {
+
+  public void print() {
+    System.out.println("Blue loafers");
+  }
+}
---
+++ Abstract_Factory/clothes/BlueShirt.java
@@ -0,0 +1,12 @@
+/**
+  * Sebastiano Valle - 23/04/2015
+  * This is the class which represents GoF's ``ProductB1" class.
+*/
+package clothes;
+
+public class BlueShirt implements Shirt {
+
+  public void print() {
+    System.out.println("Shirt with a sea picture");
+  }
+}
