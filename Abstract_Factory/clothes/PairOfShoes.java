11f12bd4ec08ad70713b53d996ea95e906b007f6
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-04-23 15:50:52 +0200

    Abstract Factory: Add Products' interfaces

---
+++ Abstract_Factory/clothes/PairOfShoes.java
@@ -0,0 +1,15 @@
+/**
+  * Sebastiano Valle - 23/04/2015
+  * This is the interface which represents GoF's ``AbstractProduct2" abstract
+  * class.
+  *
+  * Since this type is very simple (it only offers a static method), there is
+  * no need to declare PairOfShoes as an abstract class rather than an
+  * interface.
+*/
+package clothes;
+
+public interface PairOfShoes {
+
+  public void print();
+}
---
+++ Abstract_Factory/clothes/Shirt.java
@@ -0,0 +1,14 @@
+/**
+  * Sebastiano Valle - 23/04/2015
+  * This is the interface which represents GoF's ``AbstractProduct1" abstract
+  * class.
+  *
+  * Since this type is very simple (it only offers a static method), there is
+  * no need to declare Shirt as an abstract class rather than an interface.
+*/
+package clothes;
+
+public interface Shirt {
+
+  public void print();
+}
