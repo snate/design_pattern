9359472981ae6cb3cd92cf25a49e8dee8906c7a2
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-04-24 17:59:49 +0200

    Command: Add Command interface

---
+++ Command/counter/Command.java
@@ -0,0 +1,11 @@
+/**
+  * Sebastiano Valle - 24/04/2015
+  * This is the interface which represents GoF's ``Command" interface.
+  *
+  * A Command provides the functionality to be executed.
+*/
+package counter;
+
+public interface Command {
+  public void execute();
+}
